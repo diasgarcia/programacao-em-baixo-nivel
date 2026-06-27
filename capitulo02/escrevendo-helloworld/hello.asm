22a9581225cace236848351fb815e425d8bfa36e
diasgarcia <rafdgar@gmail.com>
2026-06-27 16:21:11 -0300

    hello world

---
+++ capitulo02/escrevendo-helloworld/hello.asm
@@ -0,0 +1,18 @@
+global _start
+
+section .data
+message: db 'hello, world!', 10
+
+section .text
+_start:
+
+
+    mov     rax, 1          ; o número da chamada de sistema deve ser armazewnado em rax
+    mov     rdi, 1          ; argumento #1 em dri: onde escrever (descritor)?
+    mov     rsi, message    ; argumento #2 em dsi: onde começa a string?
+    mov     rdx, 14         ; argumento #3 em rdx: quantos bytes devem ser escritos?
+    syscall
+
+    mov     rax, 60         ; syscall exit
+    xor     rdi, rdi        ; código 0
+    syscall
