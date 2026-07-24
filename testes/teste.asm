078c56e20d99c69a9c1ba3758f49f1864368c596
diasgarcia <rafdgar@gmail.com>
2026-07-23 22:34:38 -0300

    nova rotina "str_len" para a libpbn-io

--- testes/teste.asm
+++ testes/teste.asm
@@ -1,9 +1,15 @@
 %include "libpbn-io.asm"
 
+section .rodata:
+string:     db `Salve, simpatia!\n\0`
+
 section .text
 global _start
 
 _start:
 
-    mov     rdi, 0x2a   ; 42
+    mov     rdi, string
+    call    str_len
+
+    mov     rdi, rax
     call    exit
