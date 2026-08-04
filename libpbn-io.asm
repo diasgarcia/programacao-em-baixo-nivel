7f1872792d1e0900518d42cd679431a6c2269987
diasgarcia <rafdgar@gmail.com>
2026-08-03 21:54:20 -0300

    novas rotinas para a libpbn - chr_print, nl_print e começando uint64_to_str

--- libpbn-io.asm
+++ libpbn-io.asm
@@ -1,11 +1,15 @@
 %define SYS_WRITE   0X01
 %define SYS_EXIT    0x3c
 
-%define STDIN   0
-%define STDOUT  1
-%define STDERR  2
+%define FD_STDIN   0
+%define FD_STDOUT  1
+%define FD_STDERR  2
 
 %define NUL 0
+%define EOL 0x0a
+
+%define EXIT_SUCCESS 0
+%define EXIT_FAILURE 1
 
 ; ------------------------------------------------------------------------------------
 ; exit: [[noreturn]] void exit(int status{rdi});
@@ -36,7 +40,35 @@ str_print:
     call    str_len
     mov     rdx, rax
     mov     rsi, rdi
-    mov     rdi, STDOUT
+    mov     rdi, FD_STDOUT
     mov     rax, SYS_WRITE
     syscall
     ret
+
+; ------------------------------------------------------------------------------------
+; chr_print: int{rax} chr_print(int c{rdi});
+; ------------------------------------------------------------------------------------
+chr_print:
+    push    rdi
+    mov     rsi, rsp
+    mov     rdx, 1
+    mov     rax, SYS_WRITE
+    mov     rdi, FD_STDOUT
+    syscall
+    add     rsp, 8
+    ret
+
+; ------------------------------------------------------------------------------------
+; nl_print: int{rax} nl_print(void)
+; ------------------------------------------------------------------------------------
+nl_print:
+    mov     rdi, EOL
+    call    chr_print
+    ret
+
+; ------------------------------------------------------------------------------------
+; uint64_to_str: size_t{rax} uint64_to_str(char *buf{rdi}, size_t num{rsi})
+; ------------------------------------------------------------------------------------
+%define UNIT64_BUFSIZ 21
+
+uint64_to_str:
--- testes/teste.asm
+++ testes/teste.asm
@@ -1,15 +1,17 @@
 %include "libpbn-io.asm"
 
-section .rodata:
-string:     db `Salve, simpatia!\n\0`
+section .bss
+uint64_str:
+    resb    UNIT64_BUFSIZ
 
 section .text
 global _start
 
 _start:
 
-    mov     rdi, string
-    call    str_print
+    mov     rdi, 42
+    call    chr_print
+    call    nl_print
 
-    mov     rdi, rax
+    mov     rdi, EXIT_SUCCESS
     call    exit
